cc66e43cb850a63c1763935f2a2593474fdf0ff9
Megha M <megham@BLR.ALLEGISINDIA.COM>
2025-05-15 13:14:13 +0530

    fix deployment issue

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faLinkedin,
-  faGithub,
-  faConnectdevelop,
-  faMailchimp,
-} from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Footer: React.FC = () => {
   return (
